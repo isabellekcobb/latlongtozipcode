580cf4da0837889af4c3a07108e88e07b404d032
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-12 15:40:05 -0700

    Update zipcode.py

--- zipcode.py
+++ zipcode.py
@@ -12,9 +12,7 @@ def get_zip(df, lon_field, lat_field):
     location = reverse_geocode((Geometry({"x":float(df[lon_field]), "y":float(df[lat_field]), "spatialReference":{"wkid": 4326}})))
     return location['address']['Postal']
 
-df = pd.DataFrame({
-    'Lat': [29.39291, 29.39923, 29.40147, 29.38752, 29.39291, 29.39537, 29.39343, 29.39291, 29.39556],
-    'Lon': [-98.50925, -98.51256, -98.51123, -98.52372, -98.50925, -98.50402, -98.49707, -98.50925, -98.53148]
-})
-
 zipcodes = df.apply(get_zip, axis=1, lat_field='Lat', lon_field='Lon')
+
+with open('zipcodes.txt','w') as file:
+    file.write(zipcodes)
